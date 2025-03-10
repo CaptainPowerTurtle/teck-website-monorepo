630f3a2d4d82c3701f7da6905e51d145add05793
Jacob Larsen <powerturtle@protonmail.ch>
2025-03-10 22:45:06 +0000

    fix: missing files

--- apps/storybook/tailwind.config.ts
+++ apps/storybook/tailwind.config.ts
@@ -1,4 +1,4 @@
-import path from 'node:path';
+import path from "node:path";
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
@@ -6,8 +6,8 @@ const config: Config = {
   presets: [sharedConfig],
   content: [
     "./app/**/*.tsx",
-    path.join(__dirname, '**/*.{js,ts,jsx,tsx,mdx}'),
-    path.join(__dirname, '.storybook/**/*.{js,ts,jsx,tsx,mdx}'),
+    path.join(__dirname, "**/*.{js,ts,jsx,tsx,mdx}"),
+    path.join(__dirname, ".storybook/**/*.{js,ts,jsx,tsx,mdx}"),
   ],
 };
 
