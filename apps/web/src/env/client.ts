f55d60b8c77288d022d3a50512e1e7d12a14b5df
Jacob Larsen <powerturtle@protonmail.ch>
2025-03-19 22:04:02 +0000

    fix: SKIP_ENV_VALIDATION

--- apps/web/src/env/client.ts
+++ apps/web/src/env/client.ts
@@ -8,4 +8,14 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
   },
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined.
+   * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
 });
--- apps/web/src/env/server.ts
+++ apps/web/src/env/server.ts
@@ -19,4 +19,15 @@ export const env = createEnv({
   // },
   // For Next.js >= 13.4.4, you can just reference process.env:
   experimental__runtimeEnv: process.env,
+
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined.
+   * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
 });
