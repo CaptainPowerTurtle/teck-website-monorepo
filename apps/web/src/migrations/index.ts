7e75fdc451cfa20ee360b56560f32f1eff31e6b1
Jacob Larsen <jl@teck.dk>
2025-08-20 19:06:57 +0000

    feat(migrations): add users_sessions table and update related indices and columns
    - Created a new migration to add the "users_sessions" table with necessary columns and constraints.
    - Updated the "forms_emails" table to set a default subject.
    - Added new indices for the "users_sessions" table.
    - Dropped and recreated the "redirects_from_idx" index.

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250310_042530_initial from './20250310_042530_initial';
+import * as migration_20250820_184941 from './20250820_184941';
 
 export const migrations = [
   {
     up: migration_20250310_042530_initial.up,
     down: migration_20250310_042530_initial.down,
-    name: '20250310_042530_initial'
+    name: '20250310_042530_initial',
+  },
+  {
+    up: migration_20250820_184941.up,
+    down: migration_20250820_184941.down,
+    name: '20250820_184941'
   },
 ];
